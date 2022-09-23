3bb1ec2a36c3034f2d006203d03a1fc4fd025f98
Lukasz Baranowski <lukaszb48@gmail.com>
2022-09-23 10:50:40 +0100

    delete store msw database

--- src/store/store.tsx
+++ src/store/store.tsx
@@ -1,5 +1,4 @@
 import { createSlice, configureStore, createAsyncThunk } from '@reduxjs/toolkit';
-import { db } from 'mocks/db';
 import axios from 'axios';
 import { InitialPatientType } from 'types/interfaces';
 
@@ -19,53 +18,26 @@ export const fetchSinglePatient = createAsyncThunk('patient/getPatient', async (
     const response = await axios.get(`/api/patients/${id}`);
     return response.data;
   } catch (err) {
-    console.log('xxxx', err);
+    console.log(err);
   }
 });
 
-let patientFound = false;
 export const addNewPatient = createAsyncThunk('patients/addPatient', async (patient: InitialPatientType) => {
-  patientFound = false;
-  const findPatient = db.patient.findFirst({
-    where: {
-      id: {
-        equals: patient.id,
-      },
-    },
-  });
-  if (!findPatient) {
-    try {
-      const response = await axios.post('/api/patients/add', patient);
-      return response.data;
-    } catch (err) {
-      console.log(err);
-    }
-  } else if (findPatient) {
-    patientFound = true;
-    try {
-      const response = await axios.put('/api/patients/add', patient);
-      return response.data;
-    } catch (err) {
-      console.log(err);
-    }
+  console.log('AA', patient);
+  try {
+    const response = await axios.post('/api/patients', patient);
+    return response.data;
+  } catch (err) {
+    console.log(err);
   }
 });
 
-export const removePatient = createAsyncThunk('patients/removePatient', async (patient: number) => {
-  const findPatient = db.patient.findFirst({
-    where: {
-      id: {
-        equals: patient,
-      },
-    },
-  });
-  if (findPatient) {
-    try {
-      const response = await axios.delete('/api/patients', { data: findPatient });
-      return response.data;
-    } catch (err) {
-      console.log(err);
-    }
+export const removePatient = createAsyncThunk('patients/removePatient', async (id: string) => {
+  try {
+    const response = await axios.delete(`/api/patients/${id}`);
+    return response.data;
+  } catch (err) {
+    console.log(err);
   }
 });
 
@@ -75,19 +47,12 @@ const patientsListSlice = createSlice({
   reducers: {
     sortPatientsList(state, action) {
       return state.sort((a, b): any => {
-        if (action.payload === 'female') {
-          return a.sex > b.sex ? 1 : b.sex > a.sex ? -1 : 0;
-        } else if (action.payload === 'male') {
-          return a.sex > b.sex ? -1 : b.sex > a.sex ? 1 : 0;
-        } else if (action.payload === 'off') {
-          return a.id > b.id ? -1 : b.id > a.id ? 1 : 0;
-        } else if (action.payload === 'a-z') {
-          return a.surname > b.surname ? 1 : b.surname > a.surname ? -1 : 0;
-        } else if (action.payload === 'z-a') {
-          return a.surname > b.surname ? -1 : b.surname > a.surname ? 1 : 0;
-        } else {
-          return a.id > b.id ? -1 : b.id > a.id ? 1 : 0;
-        }
+        if (action.payload === 'female') return a.sex > b.sex ? 1 : b.sex > a.sex ? -1 : 0;
+        if (action.payload === 'male') return a.sex > b.sex ? -1 : b.sex > a.sex ? 1 : 0;
+        if (action.payload === 'off') return a.id > b.id ? -1 : b.id > a.id ? 1 : 0;
+        if (action.payload === 'a-z') return a.surname > b.surname ? 1 : b.surname > a.surname ? -1 : 0;
+        if (action.payload === 'z-a') return a.surname > b.surname ? -1 : b.surname > a.surname ? 1 : 0;
+        return a.id > b.id ? -1 : b.id > a.id ? 1 : 0;
       });
     },
   },
@@ -96,14 +61,10 @@ const patientsListSlice = createSlice({
       return action.payload;
     });
     builder.addCase(addNewPatient.fulfilled, (state, action) => {
-      if (!patientFound) {
-        state.push(action.payload);
-      } else if (patientFound) {
-        const findIndex = state.findIndex((patient) => patient.id === action.payload.id);
-        state[findIndex] = action.payload;
-      }
+      state.push(action.payload);
     });
     builder.addCase(removePatient.fulfilled, (state, action) => {
+      console.log(action);
       return state.filter((patient) => patient.id !== action.payload.removedPatient.id);
     });
   },
